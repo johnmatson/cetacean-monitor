2e67e1ac54105b4b41bd83dcae7c398de54eadfe
ailmayn <76503281+ailmayn@users.noreply.github.com>
2021-03-18 13:08:29 -0700

    Update model.py

--- app/model.py
+++ app/model.py
@@ -4,9 +4,9 @@ from sklearn.model_selection import train_test_split
 import tensorflow.keras as keras
 import tensorflow as tf
 
-physical_devices = tf.config.experimental.list_physical_devices('GPU')
-assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
-config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
+#physical_devices = tf.config.experimental.list_physical_devices('GPU')
+#assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+#config = tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 DATA_PATH = "app/datasets/data.json"
 
