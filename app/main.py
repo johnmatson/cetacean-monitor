26c13bf0661164556f2e8586326ef5629adc5038
John Matson <john@matson.ai>
2021-05-04 00:10:47 -0700

    DISK streaming from transmit to main (loopback mode)

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 '''
-
+Call file from command line with a mode configuration argument of
+'DISK', 'STREAM', or 'MIC' or with no arguments to run in disk mode by
+default.
 '''
 
 
@@ -8,6 +10,11 @@ import time
 import threading
 import queue
 import concurrent.futures
+import socket
+import struct
+import pickle
+import os
+import sys
 import librosa
 import numpy as np
 import tensorflow.keras as keras
@@ -24,45 +31,87 @@ MODE = 'DISK'
 AUDIO_PATH = 'app/data/full-clips/001A-short.wav'
 MODEL_PATH = 'app/saved_model'
 
-SAMPLE_RATE = 16e3
+SAMPLE_RATE = 16000
 NUM_MFCC = 13
 NUM_FFT = 2048
 HOP_LENGTH = 512
 
+HOST = '127.0.0.1'
+PORT = 6829
+
 
 def read(clip_queue, event):
     '''
-    Continuously reads data from either DISK, STREAM or MIC. Adds
-    audio data to 'clip_queue' FIFO pipeline in 1-second segments.
-    Exits when 'event' is set.
+    Continuously reads data from either DISK, STREAM or MIC. Adds audio
+    data to 'clip_queue' FIFO pipeline in 1-second segments. Exits when
+    'event' is set.
     '''
 
     if MODE == 'DISK':
 
         print('Audio file laoding...')
-        audio_file, fs = librosa.load(AUDIO_PATH)
+        audio_file, fs = librosa.load(AUDIO_PATH, sr=SAMPLE_RATE)
         print('Audio file loaded.')
 
+        end_time = time.time()
+        
         for i in range(math.floor(len(audio_file)/SAMPLE_RATE)):
-            
-            start = time.time()
 
             # add 1 second of audio to the queue
-            clip_queue.put(audio_file[int(i*SAMPLE_RATE):int(i*SAMPLE_RATE+SAMPLE_RATE)])
+            start_sample = int(i*SAMPLE_RATE)
+            end_sample = int((i+1)*SAMPLE_RATE)
+            clip_queue.put(audio_file[start_sample:end_sample])
 
             if event.is_set():
                 break
 
-            end = time.time()
-            time.sleep(1 - (end - start))
+            # sleep remainder of 1-second cycle
+            time.sleep(1 - (time.time() - end_time))
+            end_time = time.time()
+
+    if MODE == 'STREAM':
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+
+            client_socket.connect((HOST, PORT)) 
+            print("Connected to server",(HOST, PORT))
+
+            data = b""
+            payload_size = struct.calcsize("Q")
+
+            while not event.is_set():
+                
+                try:
+                    while len(data) < payload_size:
+                        packet = client_socket.recv(4*1024) # 4K
+                        if not packet: break
+                        data+=packet
+                        
+                    packed_msg_size = data[:payload_size]
+                    data = data[payload_size:]
+                    msg_size = struct.unpack("Q",packed_msg_size)[0]
+
+                    while len(data) < msg_size:
+                        data += client_socket.recv(4*1024)
+
+                    frame_data = data[:msg_size]
+                    data  = data[msg_size:]
+                    frame = pickle.loads(frame_data)
+
+                    if len(frame) >= SAMPLE_RATE:
+                        x = np.array(frame[:SAMPLE_RATE])
+                        clip_queue.put(x)
+
+                except Exception as e:
+                    print(e)
+                    break
 
 
 def process(clip_queue, event):
     '''
-    Processes audio data from 'clip_queue' as soon as it is
-    available. MFCC is calculated for each audio clip, which is
-    fed to CNN, which makes a prediction. Exits when 'event' is
-    set.
+    Processes audio data from 'clip_queue' as soon as it is available.
+    MFCC is calculated for each audio clip, which is fed to CNN, which
+    makes a prediction. Exits when 'event' is set.
     '''
 
     print('CNN model loading...')
@@ -73,7 +122,9 @@ def process(clip_queue, event):
 
         # extract mfcc
         clip = clip_queue.get()
-        mfcc_stage1 = librosa.feature.mfcc(clip, SAMPLE_RATE, n_mfcc=NUM_MFCC, n_fft=NUM_FFT, hop_length=HOP_LENGTH)
+        mfcc_stage1 = librosa.feature.mfcc(
+            clip, SAMPLE_RATE, n_mfcc=NUM_MFCC,n_fft=NUM_FFT,
+            hop_length=HOP_LENGTH)
         mfcc_stage2 = np.array((mfcc_stage1.T).tolist())
         mfcc_stage3 = mfcc_stage2[np.newaxis,...,np.newaxis]
 
@@ -83,6 +134,9 @@ def process(clip_queue, event):
 
 
 if __name__ == '__main__':
+
+    if len(sys.argv) == 2:
+        MODE = sys.argv[1]
     
     pipeline = queue.Queue(maxsize=10) # input audio queue
     event = threading.Event() # exit event flag
--- app/transmit.py
+++ app/transmit.py
@@ -1,31 +1,110 @@
 '''
-Runs on the Raspberry Pi. Reads audio from external USB
-audio interface or audio file on disk. Establishes socket
-connection with remote client and streams audio over
-connection. Call file from command line with a mode
-configuration argument of 'disk' or 'mic' or with no
-arguments to run in disk mode by default.
+Runs on the Raspberry Pi. Reads audio from external USB audio interface
+or audio file on disk. Establishes socket connection with remote client
+and streams audio over connection. Call file from command line with a
+mode configuration argument of 'DISK' or 'MIC' or with no arguments to
+run in disk mode by default.
 '''
 
 
+import socket
+import wave
+import pyaudio
+import pickle
+import struct
+import librosa
+import time
+import math
 import sys
 
 
-def transmit(mode='disk', audio_path='app/datasets/full-clips/001A-short.wav'):
+# MODE OPTIONS
+# 'DISK'    : sources audio from local audio file
+# 'MIC'     : sources audio from PC audio input
 
-    # configures module to source audio from local audio file
-    if mode == 'disk':
-        # ADD SOCKET CODE HERE
-        pass
+MODE = 'DISK'
 
-    # configures module to source audio from local audio input
-    elif mode == 'mic':
-        pass
+AUDIO_PATH = 'app/data/full-clips/001A-short.wav'
+
+HOST = '127.0.0.1'
+PORT = 6829
+
+CHUNK = 1024*4
+FORMAT = pyaudio.paFloat32
+CHANNELS = 1
+SAMPLE_RATE = 16000
+
+
+def transmit():
+    '''
+    '''
+    
+    with socket.socket() as server_socket:
+
+        server_socket.bind((HOST, PORT))
+        server_socket.listen()
+
+        if MODE == 'DISK':
+
+            print('Audio file laoding...')
+            audio_file, fs = librosa.load(AUDIO_PATH, sr=SAMPLE_RATE)
+            print('Audio file loaded.')
+
+            print('Server listening at',(HOST, PORT))
+            client_socket,addr = server_socket.accept()
+            print('Connected to clinet',addr)
+
+            end_time = time.time()
+
+            for i in range(math.floor(len(audio_file)/SAMPLE_RATE)):
+
+                if not client_socket:
+                    break
+
+                # parse 1 second of audio to stream
+                start_sample = int(i*SAMPLE_RATE)
+                end_sample = int((i+1)*SAMPLE_RATE)
+                data = audio_file[start_sample:end_sample]
+
+                # pack data for socket transmission
+                a = pickle.dumps(data)
+                message = struct.pack("Q",len(a))+a
+                client_socket.sendall(message)
+
+                # sleep remainder of 1-second cycle
+                time.sleep(1 - (time.time() - end_time))
+                end_time = time.time()
+
+        if MODE == 'MIC':
+
+            p = pyaudio.PyAudio()
+            stream = p.open(
+                format=FORMAT,
+                channels=CHANNELS,
+                rate=SAMPLE_RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+            print('Server listening at',(HOST, PORT))
+            client_socket,addr = server_socket.accept()
+            print('Connected to clinet',addr)
+
+            while client_socket:
+
+                try:
+                    data = stream.read(CHUNK)
+                    # print(data)
+                    # client_socket.sendall(stream.read(CHUNK))
+                except Exception as e:
+                    print(e)
+                    data = '\x00' * CHUNK
+
+                client_socket.sendall(data)
 
 
 if __name__ == "__main__":
 
-    if len(sys.argv) < 2:
-        transmit()
-    elif len(sys.argv) == 2:
-        transmit(mode=sys.argv[1])
+    if len(sys.argv) == 2:
+        MODE = sys.argv[1]
+        
+    transmit()
