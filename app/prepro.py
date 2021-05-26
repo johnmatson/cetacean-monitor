383270f3d4cffb034229c895231ae867a80ff25b
John Matson <john@matson.ai>
2021-05-26 13:50:04 -0700

    model.py optionally accepts separate test set

--- app/prepro.py
+++ app/prepro.py
@@ -14,15 +14,15 @@ import os
 import math
 import librosa
 
-DATASET_PATH = "app/data/datasets/main"
-JSON_PATH = "app/data/datasets/data.json"
+DATASET_PATH = 'app/data/datasets/main'
+JSON_PATH = 'app/data/datasets/data.json'
 SAMPLE_RATE = 16000
 TRACK_DURATION = 1 # measured in seconds
 SAMPLES_PER_TRACK = SAMPLE_RATE * TRACK_DURATION
 
 
 def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, num_segments=1):
-    """Extracts MFCCs from dataset and saves them into a json file along with labels.
+    '''Extracts MFCCs from dataset and saves them into a json file along with labels.
         :param dataset_path (str): Path to dataset
         :param json_path (str): Path to json file used to save MFCCs
         :param num_mfcc (int): Number of coefficients to extract
@@ -30,13 +30,13 @@ def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, 
         :param hop_length (int): Sliding window for FFT. Measured in # of samples
         :param: num_segments (int): Number of segments we want to divide sample tracks into
         :return:
-        """
+        '''
 
     # dictionary to store mapping, labels, and MFCCs
     data = {
-        "mapping": [],
-        "labels": [],
-        "mfcc": []
+        'mapping': [],
+        'labels': [],
+        'mfcc': []
     }
 
     samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
@@ -49,9 +49,9 @@ def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, 
         if dirpath is not dataset_path:
 
             # save genre label (i.e., sub-folder name) in the mapping
-            semantic_label = dirpath.split("\\")[-1]
-            data["mapping"].append(semantic_label)
-            print("\nProcessing: {}".format(semantic_label))
+            semantic_label = dirpath.split('\\')[-1]
+            data['mapping'].append(semantic_label)
+            print('\nProcessing: {}'.format(semantic_label))
 
             # process all audio files in genre sub-dir
             for f in filenames:
@@ -73,14 +73,14 @@ def save_mfcc(dataset_path, json_path, num_mfcc=13, n_fft=2048, hop_length=512, 
 
                     # store only mfcc feature with expected number of vectors
                     if len(mfcc) == num_mfcc_vectors_per_segment:
-                        data["mfcc"].append(mfcc.tolist())
-                        data["labels"].append(i-1)
-                        print("{}, segment:{}".format(file_path, d+1))
+                        data['mfcc'].append(mfcc.tolist())
+                        data['labels'].append(i-1)
+                        print('{}, segment:{}'.format(file_path, d+1))
 
     # save MFCCs to json file
-    with open(json_path, "w") as fp:
+    with open(json_path, 'w') as fp:
         json.dump(data, fp, indent=4)
         
         
-if __name__ == "__main__":
+if __name__ == '__main__':
     save_mfcc(DATASET_PATH, JSON_PATH, num_segments=1)
